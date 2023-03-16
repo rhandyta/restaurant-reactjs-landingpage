8c030bce6aef37c6c34578fef0f720589a1918f5
Rhandyta Briantama <rhandytabriantama@gmail.com>
2023-03-16 21:46:40 +0700

    rotuerl logi

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
+import Login from "../pages/Login";
 
 function Router() {
     return (
         <Routes>
-
-             <Route index element={<LandingPage />} />
-       <Route path="login" element={<Login/>}
- </Routes>
+            <Route index element={<LandingPage />} />
+            <Route path="/login" element={<Login />} />
+        </Routes>
     );
 }
 
